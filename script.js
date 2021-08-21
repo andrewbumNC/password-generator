01ee59682a5e551f2c214ac55b4d31e85c109f0e
andrew bumgarner <andrewbumgarnerNC@gmail.com>
2021-08-21 10:01:53 -0400

    updated if statements to include rrandom selectors to ensure inclusion of all desired character types

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // Variables needed at global scope
-var upperCaseOptions = ['A', 'B', 'C', 'D', 'E'];
-var lowerCaseOptions = ['a', 'b', 'c', 'd', 'e'];
-var symbolOptions = ['@', '$', '%', '*', '!'];
-var numberOptions = ['1', '2', '3', '4', '5'];
+var upperCaseOptions = ['A', 'B', 'C', 'D', 'E','F','G','H','I',];
+var lowerCaseOptions = ['a', 'b', 'c', 'd', 'e','f','g','h','i',];
+var symbolOptions = ['@', '$', '%', '*', '!','#','^','&','(',];
+var numberOptions = ['1', '2', '3', '4', '5','6','7','8','9',];
 var counter = 0;
 var password = document.querySelector("#password")
 var userSelections = [];
 var finalPassword = [];
-var userSelections = []
+var userSelections = [];
+var startOfPassword = [];
+
 
 // Click enabled on button // Need to figure out how to not include previous password in new password generated. I can't get it off screen but not from the result.
 generate.addEventListener("click", function () {
@@ -17,22 +19,19 @@ generate.addEventListener("click", function () {
   askPasswordOptions()
 })
 
-
-
 //Function used to create password
 
 function askPasswordOptions() {
   counter++
-  if (counter > 10 || counter < 0) {
-    alert("You've done this TEN times now!!! Maybe take a break.")
+  if (counter > 20 || counter < 0) {
+    alert("You've done this TWENTY times now!!! Maybe take a break.");
   } else {
     // this is for collecting all of my inputs
     
-    var upperCase = confirm("Do you want your password to contain uppercase characters?")
-    var lowerCase = confirm("Do you want your password to contain lowercase characters?")
-    var symbols = confirm("Do you want your password to contain special characters?")
-    var numbers = confirm("Do you want number characters in your password?")
-
+    var upperCase = confirm("Do you want your password to contain uppercase characters?");
+    var lowerCase = confirm("Do you want your password to contain lowercase characters?");
+    var symbols = confirm("Do you want your password to contain special characters?");
+    var numbers = confirm("Do you want number characters in your password?");
     var userLength = prompt("How long do you want your password to be");
     if (userLength < 8) {
       userLength = alert("Your password is not long enough");
@@ -41,38 +40,41 @@ function askPasswordOptions() {
       userLength = alert("Your password is too long");
       askPasswordOptions()
     }
-    // after user selects password inputs this concats each of the input arrays to a single array
+    // after user selects password inputs this concats each of the selected input arrays to a single array
 
     if (upperCase) {
+      startOfPassword = upperCaseOptions[Math.floor(Math.random() * upperCaseOptions.length)]
       userSelections = userSelections.concat(upperCaseOptions);
     }
 
     if (lowerCase) {
+      startOfPassword = startOfPassword.concat(lowerCaseOptions[Math.floor(Math.random() * lowerCaseOptions.length)])
       userSelections = userSelections.concat(lowerCaseOptions);
     }
 
     if (symbols) {
+      startOfPassword = startOfPassword.concat(symbolOptions[Math.floor(Math.random() * symbolOptions.length)])
       userSelections = userSelections.concat(symbolOptions);
     }
 
     if (numbers) {
+      startOfPassword = startOfPassword.concat(numberOptions[Math.floor(Math.random() * numberOptions.length)])
       userSelections = userSelections.concat(numberOptions);
     }
 
 
     //This randomly selects a password based on the desired user length
 
-    for (var i = 0; i < userLength; i++) {
+    for (var i = 0; i < userLength - startOfPassword.length; i++) {
       var randomNumber = Math.floor(Math.random() * userSelections.length)
       var passwordArray = userSelections[randomNumber];
       finalPassword.push(passwordArray);
     }
 
+    finalPassword = finalPassword.concat(startOfPassword);
 
     //This updated the text field on the page with the password
     password.textContent = finalPassword.join('')
-
-
   }
 }
 
